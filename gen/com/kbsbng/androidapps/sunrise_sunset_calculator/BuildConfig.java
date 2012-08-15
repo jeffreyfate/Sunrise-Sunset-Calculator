2aaf3b3cf329c39133aba4a0547eae1eee53fbf4
Keshavaprasad B S <kbsbng@gmail.com>
2012-08-15 21:54:17 +0530

    Removing files to ignored from git

--- gen/com/kbsbng/androidapps/sunrise_sunset_calculator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.kbsbng.androidapps.sunrise_sunset_calculator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
